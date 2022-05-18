7b1c38a62a79e590f384374cb7af32d34da437b4
jungdujang <jungdujang@gmail.com>
2022-05-19 02:21:30 +0900

    동작 리스트 반복 실행 기능 추가

--- src/component/Command/CommandList/OpenBrowserRow.tsx
+++ src/component/Command/CommandList/OpenBrowserRow.tsx
@@ -7,22 +7,29 @@ import {
 	InputLeftAddon,
 	Tooltip,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import useCommands from "../../../hooks/useCommands";
 import { executeCommands } from "../../../util";
 import { openBrowser } from "../../../util/ipc";
+import InputNumber from "../../InputNumber";
 
 const OpenBrowserRow: React.FC = () => {
 	const {
 		commandsState: { startUrl, connectedBrowserId, commands, parameters },
 		dispatch,
 	} = useCommands();
+	const [repeatCount, setRepeatCount] = useState(1);
 
 	const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
 		dispatch({
 			type: "SetStartUrl",
 			startUrl: e.currentTarget.value,
 		});
+	};
+
+	const handleChangeRepeatCount = (event: string) => {
+		const count = Number.parseInt(event);
+		setRepeatCount(count > 1 ? count : 1);
 	};
 
 	return (
@@ -41,6 +48,7 @@ const OpenBrowserRow: React.FC = () => {
 					disabled={!!connectedBrowserId}
 					colorScheme={"green"}
 					marginLeft="1"
+					marginRight="1"
 					onClick={async () => {
 						const { id } = await openBrowser(startUrl, () => {
 							dispatch({
@@ -58,17 +66,27 @@ const OpenBrowserRow: React.FC = () => {
 					<LinkIcon />
 				</Button>
 			</Tooltip>
+			<InputNumber
+				flex="70px 0 0"
+				min={1}
+				value={repeatCount}
+				onChange={handleChangeRepeatCount}
+			/>
 			<Tooltip label="모든 동작 실행">
 				<Button
 					disabled={!connectedBrowserId}
 					colorScheme={"green"}
-					marginLeft="1"
 					onClick={() => {
 						if (!connectedBrowserId) {
 							throw new Error("Requires connectedBrowserId");
 						}
 
-						executeCommands(connectedBrowserId, commands, parameters);
+						executeCommands(
+							connectedBrowserId,
+							commands,
+							parameters,
+							repeatCount
+						);
 					}}
 					flex="42px 0 0"
 				>
