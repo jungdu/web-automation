056949cfbd48829e7b0664ce25904d2a446ab2bf
jungdujang <jungdujang@gmail.com>
2022-05-20 02:13:06 +0900

    Update: absolute path 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,13 @@ export default defineConfig({
 	base: "./",
 	build: {
 		outDir: "dist/client",
+	},
+	resolve: {
+		alias: [
+			{
+				find: "@",
+				replacement: resolve(__dirname, "src"),
+			},
+		],
 	},
 });
